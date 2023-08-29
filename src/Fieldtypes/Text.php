7b347e010649042bf46907f91102386d6afa364f
Olly Bradshaw <hello@ollybradshaw.com>
2023-08-29 14:24:39 +0100

    Got conditional prefill working.

--- src/Fieldtypes/Text.php
+++ src/Fieldtypes/Text.php
@@ -9,19 +9,72 @@ class Text extends CoreText
     public function configFieldItems(): array
     {
         return [
-            'allow_url_prefill' => [
-                'display' => 'Allow URL Prefill',
-                'instructions' => 'When active, the field will pull the value from a GET parameter in a URL of the same name as the field handle.',
-                'type' => 'toggle',
-                'default' => false,
-                'width' => 200
+            [
+                'display' => __('Input Behavior'),
+                'fields' => [
+                    'input_type' => [
+                        'display' => __('Input Type'),
+                        'instructions' => __('statamic::fieldtypes.text.config.input_type'),
+                        'type' => 'select',
+                        'default' => 'text',
+                        'options' => [
+                            'color',
+                            'date',
+                            'email',
+                            'hidden',
+                            'month',
+                            'number',
+                            'password',
+                            'tel',
+                            'text',
+                            'time',
+                            'url',
+                            'week',
+                        ],
+                    ],
+                    'placeholder' => [
+                        'display' => __('Placeholder'),
+                        'instructions' => __('statamic::fieldtypes.text.config.placeholder'),
+                        'type' => 'text',
+                    ],
+                    'default' => [
+                        'display' => __('Default Value'),
+                        'instructions' => __('statamic::messages.fields_default_instructions'),
+                        'type' => 'text',
+                    ],
+                    'character_limit' => [
+                        'display' => __('Character Limit'),
+                        'instructions' => __('statamic::fieldtypes.text.config.character_limit'),
+                        'type' => 'integer',
+                    ],
+                ],
+            ],
+            [
+                'display' => __('Advanced Form Builder'),
+                'fields' => [
+                    'allow_url_prefill' => [
+                        'display' => 'Allow URL Prefill',
+                        'instructions' => 'When active, the field will pull the value from a GET parameter in a URL of the same name as the field handle.',
+                        'type' => 'toggle',
+                        'default' => false,
+                        'width' => 200
+                    ]
+                ]
             ]
         ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function selectable() : bool
+    {
+        return 0;
     }
     
     public function view()
     {
-        $default = 'statamic::forms.fields.'.$this->handle();
+        $default = 'statamic-advanced-forms::fieldtypes.text';
 
         return view()->exists($default)
             ? $default
